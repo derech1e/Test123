2616731ecd4f766a3595a3b9a376afc46253b42c
Thomas Nürk <jojopaul88@gmail.com>
2021-02-05 22:26:07 +0100

    Initial Commit

---
+++ src/main/java/de/Thomas/main/Main.java
@@ -0,0 +1,12 @@
+package de.Thomas.main;
+
+/**
+ * @author derech1e
+ * @since 05.02.2021
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
